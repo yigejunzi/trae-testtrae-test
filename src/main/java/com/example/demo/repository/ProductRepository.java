aa75a6c8cdc63d53402f29db9591ccfac063e5a3
User Name <user@example.com>
2025-08-04 10:44:54 +0800

    Commit changes before merging to master

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductRepository extends ElasticsearchRepository<Product, String> {
-    // Spring Data will automatically implement this interface
-    // We can add custom query methods here if needed
-}
